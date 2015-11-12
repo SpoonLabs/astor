8af0160c3db61d72a9809e4acc88742aa682f371
martinezmatias <matias.martinez@inria.fr>
2015-11-12 16:33:47 +0100

    Adding MutRepair test

--- src/test/java/fr/inria/astor/test/repair/evaluation/MutRepairTest.java
+++ src/test/java/fr/inria/astor/test/repair/evaluation/MutRepairTest.java
@@ -5,35 +5,36 @@ import java.util.Arrays;
 
 import org.junit.Test;
 
+import fr.inria.astor.core.setup.ConfigurationProperties;
 import fr.inria.main.AbstractMain;
 import fr.inria.main.evolution.AstorMain;
-
+/**
+ * MutRepair test
+ * @author Matias Martinez matias.martinez@inria.fr
+ *
+ */
 public class MutRepairTest extends BaseEvolutionaryTest {
-
+	
+	
 	@Test
-	public void Math280() throws Exception {
+	public void testMath85issue280() throws Exception {
 		AstorMain main1 = new AstorMain();
-		File out = new File("./outputMutation/");
-			String[] args = new String[] {
-				"-dependencies", new File("examples/Math-issue-280/lib/junit-4.4.jar").getAbsolutePath(),
-				"-mode","mutation",
-				"-failing", 
-				"org.apache.commons.math.distribution.NormalDistributionTest", 
-				"-package", "org",
-				"-location",new File("./examples/Math-issue-280/").getAbsolutePath(),							
-				"-jvm4testexecution", "/home/matias/develop/jdk1.7.0_71/bin",
-				"-srcjavafolder", "/src/java/",
-				"-srctestfolder", "/test/", 
-				"-binjavafolder", "/target/classes/", 
-				"-bintestfolder","/target/test-classes/",
-				"-flthreshold","0.5",
-				"-out",out.getAbsolutePath()
-				};
+		String dep = new File("./examples/math_85/libs/junit-4.4.jar").getAbsolutePath();//"/home/matias/.m2/repository/junit/junit/4.4/junit-4.4.jar";
+
+		String[] args = new String[] { 
+				"-dependencies", dep, "-mode", "mutation", "-failing",
+				"org.apache.commons.math.distribution.NormalDistributionTest", "-location",
+				new File("./examples/math_85").getAbsolutePath(), "-package", "org.apache.commons",
+				"-jvm4testexecution", "/home/matias/develop/jdk1.7.0_71/bin", "-srcjavafolder", "/src/java/",
+				"-srctestfolder", "/src/test/", "-binjavafolder", "/target/classes", "-bintestfolder",
+				"/target/test-classes", "-javacompliancelevel", "7", "-flthreshold", "0.1", "-stopfirst", "false",
+				"-maxgen", "100"};
 		System.out.println(Arrays.toString(args));
 		main1.main(args);
-		//validatePatchExistence(ConfigurationProperties.getProperty("workingDirectory")+File.separator+"MainjGenProg-testKaliRemoveStatement/",1);
+		validatePatchExistence(ConfigurationProperties.getProperty("workingDirectory")+File.separator+"AstorMain-math_85/",1);
 		
 	}
+	
 	
 	@Test
 	public void testMath288() throws Exception {
@@ -63,6 +64,31 @@ public class MutRepairTest extends BaseEvolutionaryTest {
 	@Override
 	public AbstractMain createMain() {
 		return  new AstorMain();
+	}
+	
+	@Deprecated
+	public void Math280() throws Exception {
+		AstorMain main1 = new AstorMain();
+		File out = new File("./outputMutation/");
+			String[] args = new String[] {
+				"-dependencies", new File("examples/Math-issue-280/lib/junit-4.4.jar").getAbsolutePath(),
+				"-mode","mutation",
+				"-failing", 
+				"org.apache.commons.math.distribution.NormalDistributionTest", 
+				"-package", "org",
+				"-location",new File("./examples/Math-issue-280/").getAbsolutePath(),							
+				"-jvm4testexecution", "/home/matias/develop/jdk1.7.0_71/bin",
+				"-srcjavafolder", "/src/java/",
+				"-srctestfolder", "/test/", 
+				"-binjavafolder", "/target/classes/", 
+				"-bintestfolder","/target/test-classes/",
+				"-flthreshold","0.5",
+				"-out",out.getAbsolutePath()
+				};
+		System.out.println(Arrays.toString(args));
+		main1.main(args);
+		//validatePatchExistence(ConfigurationProperties.getProperty("workingDirectory")+File.separator+"MainjGenProg-testKaliRemoveStatement/",1);
+		
 	}
 	
 	@Override
