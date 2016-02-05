81035e182a6e82f2e846c0578d87cbcac139a5f1
martinezmatias <matias.sebastian.martinez@gmail.com>
2016-02-05 15:06:14 +0100

    Adding introclass example (3b2376_003) and test that executes it (but it does not assert the output)

--- src/test/java/fr/inria/astor/test/repair/evaluation/IntroClassTest.java
+++ src/test/java/fr/inria/astor/test/repair/evaluation/IntroClassTest.java
@@ -2,19 +2,27 @@ package fr.inria.astor.test.repair.evaluation;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
+
 import org.junit.Test;
 
 import fr.inria.main.evolution.AstorMain;
 
-
+/**
+ * 
+ * @author matias
+ *
+ */
 public class IntroClassTest {
 
 	@Test
-	public void hola() throws Exception {
+	public void test_3b2376_003() throws Exception {
+		String dep = new File("./examples/libs/junit-4.11.jar").getAbsolutePath();
 		String[] command = new String[]{
-		"-location", "/Users/matias/develop/IntroClassJava/dataset/median/3b2376ab97bb5d1a5dbbf2b45cf062db320757549c761936d19df05e856de894e45695014cd8063cdc22148b13fa1803b3c9e77356931d66f4fbec0efacf7829/003/",
+		"-location", 
+		new File("./examples/introclass/3b2376ab97bb5d1a5dbbf2b45cf062db320757549c761936d19df05e856de894e45695014cd8063cdc22148b13fa1803b3c9e77356931d66f4fbec0efacf7829/003/").getAbsolutePath(),
 		"-failing","introclassJava.median_3b2376ab_003BlackboxTest:introclassJava.median_3b2376ab_003WhiteboxTest", 
-		"-dependencies", "/Users/matias/develop/code/astor/examples/libs/junit-4.11.jar",
+		"-dependencies", dep,
 		"-seed","10",
 		"-skipfaultlocalization",
 		};
