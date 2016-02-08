5a79a859bdc16748c44bd19c9a80968a1ca88adb
martinezmatias <matias.sebastian.martinez@gmail.com>
2016-02-08 07:34:54 +0100

    Adding test for -skipFaultlocalization option

--- src/test/java/fr/inria/astor/test/repair/evaluation/IntroClassTest.java
+++ src/test/java/fr/inria/astor/test/repair/evaluation/IntroClassTest.java
@@ -1,11 +1,17 @@
 package fr.inria.astor.test.repair.evaluation;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
+import org.junit.Assert;
 import org.junit.Test;
 
+import fr.inria.astor.core.entities.ProgramVariant;
+import fr.inria.astor.core.loop.evolutionary.JGenProg;
+import fr.inria.astor.core.util.ProcessUtil;
+import fr.inria.main.ExecutionMode;
 import fr.inria.main.evolution.AstorMain;
 
 /**
@@ -15,6 +21,9 @@ import fr.inria.main.evolution.AstorMain;
  */
 public class IntroClassTest {
 
+	public static Logger log = Logger.getLogger(IntroClassTest.class.getName());
+	
+	
 	@Test
 	public void test_3b2376_003() throws Exception {
 		String dep = new File("./examples/libs/junit-4.11.jar").getAbsolutePath();
@@ -29,4 +38,67 @@ public class IntroClassTest {
 		AstorMain.main(command);
 	}
 
+	@Test
+	public void test_3b2376_003FaultLocalization() throws Exception {
+	
+		
+		log.debug("\nInit Skip fault localization test");
+		double thfl = 0.001;
+		int gensLowThr = genNumberGensForIntroClass3b2376_003(thfl,false);
+		assertTrue(gensLowThr > 0);
+		
+		thfl=0.7d;
+		int gensHighThr = genNumberGensForIntroClass3b2376_003(thfl,false);
+		assertTrue(gensHighThr > 0);
+		assertTrue(gensLowThr > gensHighThr);
+		
+		
+		thfl=0.0d;
+		int gensAll = genNumberGensForIntroClass3b2376_003(thfl,true);
+		assertTrue(gensAll > 0);
+		assertTrue(gensAll > gensLowThr);
+		assertTrue(gensAll > gensHighThr);
+		
+		
+	}
+
+	private int genNumberGensForIntroClass3b2376_003(double thfl, boolean skipFL) throws Exception {
+		String dependenciespath = new File("./examples/libs/junit-4.11.jar").getAbsolutePath() 
+				+ File.pathSeparator
+				+ new File("./examples/Math-0c1ef/lib/hamcrest-core-1.3.jar").getAbsolutePath();
+		String projectId = "IntroClass003";
+		String failing = "introclassJava.median_3b2376ab_003BlackboxTest:introclassJava.median_3b2376ab_003WhiteboxTest";
+		File exampleLocation = new File("./examples/introclass/3b2376ab97bb5d1a5dbbf2b45cf062db320757549c761936d19df05e856de894e45695014cd8063cdc22148b13fa1803b3c9e77356931d66f4fbec0efacf7829/003/");
+		String location = exampleLocation.getAbsolutePath();
+		String packageToInstrument = "";
+		
+		
+		String[] command = new String[]{"-dependencies",dependenciespath,"-location",location,
+				"-flthreshold",Double.toString(thfl),
+				"-package",packageToInstrument,
+				"-failing", failing,
+				"-id",projectId,
+				"-population","1",
+				"-seed", "10", ((skipFL)?"-skipfaultlocalization":"")
+				};
+		
+
+		AstorMain main = new AstorMain();
+		
+		boolean correctArguments = main.processArguments(command);
+		assertTrue(correctArguments);
+		
+		main.initProject(location, projectId, dependenciespath, packageToInstrument, thfl, failing);
+
+		JGenProg jgp = main.createEngine(ExecutionMode.JGenProg);
+		
+		jgp.createInitialPopulation();
+		
+		Assert.assertEquals(1, jgp.getVariants().size());
+
+		ProgramVariant variant = jgp.getVariants().get(0);
+		int nroGen = variant.getGenList().size();
+		return nroGen;
+	}
+	
 }
