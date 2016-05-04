5a814b7054bc6faed92b8904c4b8351829b6aeb1
martinezmatias <matias.sebastian.martinez@gmail.com>
2016-05-04 10:18:18 +0200

    Introduction of StatementTransformer
    Many of Repair Operators across different repair approaches do the same
    model transformation. Example: remove or replace a statement. The idea
    is to delegate those model transformation to the Transformation class.
    (Older version of Astor works using a similar organization)

---
+++ src/main/java/fr/inria/astor/core/manipulation/modelTransformer/StatamentTransformer.java
@@ -0,0 +1,101 @@
+package fr.inria.astor.core.manipulation.modelTransformer;
+
+import org.apache.log4j.Logger;
+
+import fr.inria.astor.core.entities.ModificationInstance;
+import fr.inria.astor.core.entities.ProgramVariant;
+import spoon.reflect.code.CtBlock;
+import spoon.reflect.code.CtStatement;
+
+/**
+ * Summarization of Model transformation.
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class StatamentTransformer {
+	
+	protected static Logger log = Logger.getLogger(StatamentTransformer.class.getName());
+	
+	
+	public static boolean doReplaceStatement(ModificationInstance operation){
+		boolean successful = false;
+		CtStatement ctst = (CtStatement) operation.getOriginal();
+		CtStatement fix = (CtStatement) operation.getModified();
+
+		CtBlock parentBlock = operation.getParentBlock();
+
+		if (parentBlock != null) {
+
+			try {
+				ctst.replace((CtStatement) fix);
+				fix.setParent(parentBlock);
+				successful = true;
+				operation.setSuccessfulyApplied(successful);
+			} catch (Exception ex) {
+				log.error("Error applying an operation, exception: " + ex.getMessage());
+				operation.setExceptionAtApplied(ex);
+				operation.setSuccessfulyApplied(false);
+			}
+		} else {
+			log.error("Operation not applied. Parent null ");
+		}
+		return successful;
+	}
+	
+	public static boolean undoReplaceStatement(ModificationInstance operation) {
+		CtStatement ctst = (CtStatement) operation.getOriginal();
+		CtStatement fix = (CtStatement) operation.getModified();
+		CtBlock<?> parentBlock = operation.getParentBlock();
+		if (parentBlock != null) {
+				fix.replace((CtStatement) ctst);
+				return true;
+
+		}
+		return false;
+	}
+	
+	public static boolean doRemoveStatement(ModificationInstance operation){
+		boolean successful = false;
+		CtStatement ctst = (CtStatement) operation.getOriginal();
+	
+		CtBlock parentBlock = operation.getParentBlock();
+
+		if (parentBlock != null) {
+
+			try {
+				parentBlock.getStatements().remove(ctst);
+				successful = true;
+				operation.setSuccessfulyApplied(successful);
+			} catch (Exception ex) {
+				log.error("Error applying an operation, exception: " + ex.getMessage());
+				operation.setExceptionAtApplied(ex);
+				operation.setSuccessfulyApplied(false);
+			}
+		} else {
+			log.error("Operation not applied. Parent null ");
+		}
+		return successful;
+	}
+	
+
+	public static boolean undoRemoveStatement(ModificationInstance operation) {
+		CtStatement ctst = (CtStatement) operation.getOriginal();
+		CtBlock<?> parentBlock = operation.getParentBlock();
+		if (parentBlock != null) {
+			if ((parentBlock.getStatements().isEmpty() && operation.getLocationInParent() == 0)
+					|| (parentBlock.getStatements().size() >= operation.getLocationInParent())) {
+				
+				parentBlock.getStatements().add(operation.getLocationInParent(), ctst);
+				return true;
+			} else {
+				log.error("Problems to recover, re-adding " + ctst + " at location " + operation.getLocationInParent()
+						+ " from parent size " + parentBlock.getStatements().size());
+				throw new IllegalStateException("Undo:Not valid index");
+			}
+
+		}
+		return false;
+	}
+	
+}
