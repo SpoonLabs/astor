146952ffa2f9be1ca4449c495d2219bca3adc6c4
martinezmatias <matias.sebastian.martinez@gmail.com>
2016-05-22 17:44:57 +0200

    Minor: improvement in junit executor

--- src/main/java/fr/inria/astor/junitexec/JUnitTestExecutor.java
+++ src/main/java/fr/inria/astor/junitexec/JUnitTestExecutor.java
@@ -13,7 +13,6 @@ import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 import org.junit.runners.model.InitializationError;
 
-
 /**
  * This class runs a JUnit test suite i.e., a set of test cases.
  * 
@@ -26,18 +25,17 @@ public class JUnitTestExecutor {
 
 	List<String> failTest = new ArrayList<String>();
 
-	public static String OUTSEP = "mmout";
+	public final static String OUTSEP = "mmout";
 
-	private void runinBackground(List<Class> testToRun) throws InitializationError, FileNotFoundException {
+	
 
-		JUnitCore runner = new JUnitCore();
-		Result r = runner.run(testToRun.toArray(new Class[testToRun.size()]));
+	public String createOutput(Result r) {
 		String out = "[";
 		int count = 0;
 		try {
 			for (Failure f : r.getFailures()) {
 				String s = failureMessage(f);
-				//if(s.startsWith("warning"))
+				// if(s.startsWith("warning"))
 				out += s + "-,";
 				count++;
 				if (count > 10) {
@@ -46,12 +44,10 @@ public class JUnitTestExecutor {
 				}
 			}
 		} catch (Exception e) {
-			//We do not care about this exception, 
+			// We do not care about this exception,
 		}
 		out = out.substring(0, out.length() - 1) + "]";
-		// This sysout is necessary for the communication between process...
-		System.out.println(OUTSEP + r.getRunCount() + OUTSEP + r.getFailureCount() + OUTSEP + out + OUTSEP);
-
+		return (OUTSEP + r.getRunCount() + OUTSEP + r.getFailureCount() + OUTSEP + out + OUTSEP);
 	}
 
 	private String failureMessage(Failure f) {
@@ -62,44 +58,27 @@ public class JUnitTestExecutor {
 		}
 	}
 
-	public Result runTest(String testName) throws FileNotFoundException, ClassNotFoundException, InitializationError {
-
-		Result resultTestRun;
-		try {
-
-			Class cReflect = null;
-			cReflect = Thread.currentThread().getContextClassLoader().loadClass(testName);
-
-			JUnitCore runner = new JUnitCore();
-			resultTestRun = runner.run(cReflect);
-
-			return resultTestRun;
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			throw (e);
-		}
-	}
-
-	/**
-	 * @return the class with the given qualified name.
-	 */
-	@SuppressWarnings("unchecked")
-	public <T> Class<T> loadURL(String qualifiedName, String pathClasspath) {
-		try {
-			URL url = new File(pathClasspath).toURI().toURL();
-			URLClassLoader cl = new URLClassLoader(new URL[] { url });
-			Thread.currentThread().setContextClassLoader(cl);
-			return (Class<T>) (cl.loadClass(qualifiedName));
-		} catch (MalformedURLException e) {
-			throw new RuntimeException(e);
-		} catch (ClassNotFoundException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
 	public static void main(String[] arg) throws Exception, InitializationError {
 
 		JUnitTestExecutor re = new JUnitTestExecutor();
+
+		Result result = re.run(arg);
+		// This sysout is necessary for the communication between process...
+		System.out.println(re.createOutput(result));
+
+		System.exit(0);
+	}
+
+	public Result run(String[] arg) throws Exception {
+	
+		List<Class> classes = getClassesToRun(arg);
+		JUnitCore runner = new JUnitCore();
+		Result resultjUnit = runner.run(classes.toArray(new Class[classes.size()]));
+
+		return resultjUnit;
+	}
+
+	private List<Class> getClassesToRun(String[] arg) throws ClassNotFoundException {
 		TestFilter tf = new TestFilter();
 		List<Class> classes = new ArrayList<Class>();
 		for (int i = 0; i < arg.length; i++) {
@@ -110,8 +89,7 @@ public class JUnitTestExecutor {
 			}
 
 		}
-		re.runinBackground(classes);
-		System.exit(0);
+		return classes;
 	}
 
 }
