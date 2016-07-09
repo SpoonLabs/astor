d658df9b9073a8fa8e779777e6a3f2ff75e042d5
martinezmatias <matias.sebastian.martinez@gmail.com>
2016-07-09 11:33:06 +0200

    New test cases
    Regression + steeply step.

--- src/test/java/fr/inria/astor/test/repair/evaluation/ValidationTest.java
+++ src/test/java/fr/inria/astor/test/repair/evaluation/ValidationTest.java
@@ -11,7 +11,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import fr.inria.astor.core.entities.ProgramVariant;
+import fr.inria.astor.core.entities.ProgramVariantValidationResult;
 import fr.inria.astor.core.setup.ConfigurationProperties;
+import fr.inria.astor.core.validation.validators.EvoSuiteValidationResult;
+import fr.inria.astor.core.validation.validators.RegressionValidation;
 import fr.inria.main.evolution.AstorMain;
 
 /**
@@ -44,6 +48,45 @@ public class ValidationTest {
 				//
 				"ignoredtestcases", "org.apache.commons.lang.LocaleUtilsTest",
 
+				//
+				
+				
+		};
+		try {
+			System.out.println(Arrays.toString(args));
+			LogManager.getRootLogger().setLevel(Level.DEBUG);
+			main1.execute(args);
+			System.out.println("Solution SetbyStep " + main1.getEngine().getSolutions().size());
+			//This assertiong was failing at travis CI
+			// assertTrue(main1.getEngine().getSolutions().size() > 0);
+				
+		} catch (Exception e) {
+			System.out.println("StepByStep fails " + e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	
+
+	//@Test
+	public void testLang63RegresionValidationStepbyStep() throws Exception {
+		AstorMain main1 = new AstorMain();
+		String dep = new File("./examples/libs/junit-3.8.1.jar").getAbsolutePath();
+		File out = new File(ConfigurationProperties.getProperty("workingDirectory"));
+		String[] args = new String[] { "-dependencies", dep, "-mode", "statement", "-failing",
+				"org.apache.commons.lang.time.DurationFormatUtilsTest"
+				// + ":org.apache.commons.lang.builder.ToStringBuilder",
+				, "-location", new File("./examples/lang_63/").getAbsolutePath(),
+				//
+				"-package", "org.apache.commons", "-srcjavafolder", "/src/main/java/", "-srctestfolder",
+				"/src/test/java", "-binjavafolder", "/target/classes", "-bintestfolder", "/target/test-classes",
+				"-javacompliancelevel", "7", "-flthreshold", "0.5", "-out", out.getAbsolutePath(), "-scope", "local",
+				"-seed", "6010", "-maxgen", "50", "-stopfirst", "true", "-maxtime", "30", "-testbystep",
+				//
+				"ignoredtestcases", "org.apache.commons.lang.LocaleUtilsTest",
+				//Here, we specify the regression Validation
+				"-validation", RegressionValidation.class.getCanonicalName(),
+
 		};
 		try {
 			System.out.println(Arrays.toString(args));
@@ -51,12 +94,35 @@ public class ValidationTest {
 			main1.execute(args);
 			System.out.println("Solution SetbyStep " + main1.getEngine().getSolutions().size());
 			// assertTrue(main1.getEngine().getSolutions().size() > 0);
+			
+			ProgramVariant variantSolution = main1.getEngine().getSolutions().get(0);
+			ProgramVariantValidationResult validationResult = variantSolution.getValidationResult();
+		
+			
+			assertNotNull("Without validation",validationResult);
+			//As we execute jgp in evosuite validation mode, we expect eSvalidationResult
+			assertTrue(validationResult instanceof EvoSuiteValidationResult);
+			EvoSuiteValidationResult esvalidationresult = (EvoSuiteValidationResult) validationResult;
+			//The main validation must be true (due it is a solution)
+			assertTrue(esvalidationresult.getFailingTestValidation().wasSuccessful());
+			
+			assertTrue(esvalidationresult.getManualTestValidation().wasSuccessful());
+			
+			assertNotNull(esvalidationresult.getEvoValidation());
+			
+			assertTrue(esvalidationresult.getEvoValidation().isRegressionExecuted());
+			
+			assertTrue(esvalidationresult.getEvoValidation().getCasesExecuted() > 0);
+			
+			
 		} catch (Exception e) {
 			System.out.println("StepByStep fails " + e.getMessage());
 			e.printStackTrace();
+			fail();
 		}
 	}
 
+	
 	@After
 	public void teardown() {
 		LogManager.getRootLogger().setLevel(Level.ERROR);
