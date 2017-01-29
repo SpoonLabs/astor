6eccf1b82a1acc2bb42fd32832f503ea1fb7fcfa
martinezmatias <matias.sebastian.martinez@gmail.com>
2017-01-29 14:56:43 +0100

    genetic programming
    Adding crossover operator

--- src/main/java/fr/inria/astor/approaches/jgenprog/JGenProg.java
+++ src/main/java/fr/inria/astor/approaches/jgenprog/JGenProg.java
@@ -22,6 +22,7 @@ import fr.inria.astor.core.manipulation.sourcecode.BlockReificationScanner;
 import fr.inria.astor.core.setup.ConfigurationProperties;
 import fr.inria.astor.core.setup.FinderTestCases;
 import fr.inria.astor.core.setup.ProjectRepairFacade;
+import fr.inria.astor.core.setup.RandomManager;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.CtType;
@@ -272,6 +273,61 @@ public class JGenProg extends AstorCoreEngine {
 
 	public void setIngredientStrategy(IngredientSearchStrategy ingredientStrategy) {
 		this.ingredientSearchStrategy = ingredientStrategy;
+	}
+
+	@Override
+	public void prepareNextGeneration(List<ProgramVariant> temporalInstances, int generation) {
+		
+		super.prepareNextGeneration(temporalInstances, generation);
+		if(false || ConfigurationProperties.getPropertyBool("applyCrossover")){
+			int numberVariants = this.variants.size();
+			if(numberVariants <= 1){
+				log.debug("CO|Not Enough variants to apply Crossover");
+				return;
+			}
+			
+			//We randomly choose the two variants to crossover
+			ProgramVariant v1 = this.variants.get(RandomManager.nextInt(numberVariants));
+			ProgramVariant v2 = this.variants.get(RandomManager.nextInt(numberVariants));
+			//Same instance
+			if(v1 == v2){
+				log.debug("CO|randomless chosen the same variant to apply crossover");
+				return;
+			}
+			
+			if(v1.getOperations().isEmpty() || v2.getOperations().isEmpty()){
+				log.debug("CO|Not Enough ops to apply Crossover");
+				return;
+			}
+			//we randomly select the generations to apply
+			int rgen1index = RandomManager.nextInt(v1.getOperations().keySet().size()) + 1;
+			int rgen2index = RandomManager.nextInt(v2.getOperations().keySet().size()) + 1;
+			
+			List<OperatorInstance> ops1 = v1.getOperations((int) v1.getOperations().keySet().toArray()[rgen1index]);
+			List<OperatorInstance> ops2 = v2.getOperations((int) v2.getOperations().keySet().toArray()[rgen2index]);
+	
+			OperatorInstance opinst1 = ops1.remove((int)RandomManager.nextInt(ops1.size()));
+			OperatorInstance opinst2 = ops2.remove((int)RandomManager.nextInt(ops2.size()));
+			
+			if(opinst1 == null || opinst2 == null){
+				log.debug("CO|We could not retrieve a operator");
+				return;
+			}
+			
+			//The generation of both new operators is the Last one.
+			//In the first variant we put the operator taken from the 2 one.
+			v1.putModificationInstance(generation, opinst2);
+			//In the second variant we put the operator taken from the 1 one.
+			v2.putModificationInstance(generation, opinst1);
+			//
+		}
+	}
+
+	@Override
+	public void showResults() {
+		super.showResults();
+		log.info("ing_attempts:("+this.currentStat.ingAttemps.size()
+				+ ") "+this.currentStat.ingAttemps);
 	}
 
 
