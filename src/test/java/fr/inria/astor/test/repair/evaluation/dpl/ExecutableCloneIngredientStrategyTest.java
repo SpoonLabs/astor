ab775ffd76b445d6e26871603ee72dcbc8f29b92
martinezmatias <matias.sebastian.martinez@gmail.com>
2017-01-30 10:04:42 +0100

    Adding test cases
    Two tests: one for CtType granularity, other for CtExecutable

---
+++ src/test/java/fr/inria/astor/test/repair/evaluation/dpl/ExecutableCloneIngredientStrategyTest.java
@@ -0,0 +1,145 @@
+package fr.inria.astor.test.repair.evaluation.dpl;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Queue;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import fr.inria.astor.approaches.jgenprog.JGenProg;
+import fr.inria.astor.approaches.jgenprog.operators.ReplaceOp;
+import fr.inria.astor.core.entities.Ingredient;
+import fr.inria.astor.core.entities.ModificationPoint;
+import fr.inria.astor.core.entities.ProgramVariant;
+import fr.inria.astor.core.loop.spaces.ingredients.IngredientSearchStrategy;
+import fr.inria.astor.core.loop.spaces.ingredients.IngredientSpace;
+import fr.inria.astor.core.loop.spaces.ingredients.ingredientSearch.CloneIngredientSearchStrategy;
+import fr.inria.astor.core.loop.spaces.ingredients.scopes.CtLocationIngredientSpace;
+import fr.inria.astor.core.loop.spaces.operators.AstorOperator;
+import fr.inria.astor.core.setup.ConfigurationProperties;
+import fr.inria.main.evolution.AstorMain;
+import spoon.reflect.code.CtCodeElement;
+import spoon.reflect.declaration.CtExecutable;
+import spoon.reflect.declaration.CtType;
+
+/**
+ * Test for Clone ingredient strategy
+ * 
+ * @author Matias Martinez
+ *
+ */
+public class ExecutableCloneIngredientStrategyTest {
+
+	protected Logger log = Logger.getLogger(this.getClass().getName());
+
+	@Test
+	public void testInputProperties() throws ClassNotFoundException, IOException {
+
+		ClassLoader classLoader = getClass().getClassLoader();
+		File learningDir = new File(classLoader.getResource("learningm70").getFile());
+
+		assertTrue(learningDir.exists());
+
+		assertTrue(learningDir.list().length > 0);
+
+		ConfigurationProperties.properties.setProperty("learningdir", learningDir.getAbsolutePath());
+		ConfigurationProperties.properties.setProperty("clonegranularity", CtType.class.getCanonicalName());
+		IngredientSpace ingredientSpace = null;
+		CloneIngredientSearchStrategy<?> cloneStrategy = new CloneIngredientSearchStrategy<>(ingredientSpace);
+
+	}
+	private String dep = new File("./examples/libs/junit-4.4.jar").getAbsolutePath();
+	private	File out = new File(ConfigurationProperties.getProperty("workingDirectory"));
+
+	
+	
+	@Test
+	public void testExecutableMath70() throws Exception{
+
+		// Now, let's test at Executanble granularity
+
+		Class executableCloneGranularity = CtExecutable.class;
+		
+		ClassLoader classLoader = getClass().getClassLoader();
+		File learningDir = new File(classLoader.getResource("learningm70").getFile());
+
+		Class typeCloneGranularityClass = CtExecutable.class;
+		String[] args = createCommandM70(dep, out, learningDir, executableCloneGranularity);
+
+		log.debug(Arrays.toString(args));
+
+		AstorMain main1 = new AstorMain();
+		main1.execute(args);
+		JGenProg engine = (JGenProg) main1.getEngine();
+
+		ProgramVariant pvariant = engine.getVariants().get(0);
+
+		ModificationPoint buggyStatementModPoint = pvariant.getModificationPoints().get(0);
+
+		assertEquals(72, buggyStatementModPoint.getCodeElement().getPosition().getEndLine());
+
+		CloneIngredientSearchStrategy<CtExecutable> excloneStrategy = (CloneIngredientSearchStrategy<CtExecutable>) engine
+				.getIngredientStrategy();
+
+		AstorOperator op = new ReplaceOp();
+		Ingredient exingredientSelected = excloneStrategy.getFixIngredient(buggyStatementModPoint, op);
+
+		assertNotNull(exingredientSelected);
+
+		CtExecutable<?> executParent = buggyStatementModPoint.getCodeElement().getParent(CtExecutable.class);
+
+		Queue<CtCodeElement> execingredientsToConsider = excloneStrategy.getfixspace(buggyStatementModPoint, op,
+				executParent);
+
+		assertNotNull(execingredientsToConsider);
+
+		assertFalse(execingredientsToConsider.isEmpty());
+
+		log.debug(execingredientsToConsider);
+		
+	
+
+	}
+
+	static public String[] createCommandM70(String dep, File out, File learningDir, Class cloneGranularityClass) {
+		String[] args = new String[] { "-dependencies", dep, "-mode", "statement",
+				//
+				"-failing", "org.apache.commons.math.analysis.solvers.BisectionSolverTest",
+				//
+				"-location", new File("./examples/math_70").getAbsolutePath(),
+				//
+				"-package", "org.apache.commons", "-srcjavafolder",
+				//
+				"/src/java/", "-srctestfolder", "/src/test/", "-binjavafolder", "/target/classes", "-bintestfolder",
+				"/target/test-classes",
+				//
+				"-javacompliancelevel", "7", "-flthreshold", "0.5", "-out", out.getAbsolutePath(),
+				//
+				"-scope", CtLocationIngredientSpace.class.getCanonicalName(),
+				//
+				"-seed", "10",
+				// HERE WE FORCE TO NOT EVOLVE
+				"-maxgen", "0",
+				//
+				"-stopfirst", "true",
+				//
+				"-maxtime", "100",
+				//
+				// Learning Arguments
+				"-learningdir", learningDir.getAbsolutePath(),
+				//
+				"-clonegranularity", cloneGranularityClass.getCanonicalName(),
+				//
+
+				"-ingredientstrategy", CloneIngredientSearchStrategy.class.getName()
+
+		};
+		return args;
+	}
+}
---
+++ src/test/java/fr/inria/astor/test/repair/evaluation/dpl/TypeCloneIngredientStrategyTestTest.java
@@ -0,0 +1,112 @@
+package fr.inria.astor.test.repair.evaluation.dpl;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Queue;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+
+import fr.inria.astor.approaches.jgenprog.JGenProg;
+import fr.inria.astor.approaches.jgenprog.operators.InsertAfterOp;
+import fr.inria.astor.approaches.jgenprog.operators.ReplaceOp;
+import fr.inria.astor.core.entities.Ingredient;
+import fr.inria.astor.core.entities.ModificationPoint;
+import fr.inria.astor.core.entities.ProgramVariant;
+import fr.inria.astor.core.loop.spaces.ingredients.IngredientSearchStrategy;
+import fr.inria.astor.core.loop.spaces.ingredients.IngredientSpace;
+import fr.inria.astor.core.loop.spaces.ingredients.ingredientSearch.CloneIngredientSearchStrategy;
+import fr.inria.astor.core.loop.spaces.operators.AstorOperator;
+import fr.inria.astor.core.setup.ConfigurationProperties;
+import fr.inria.main.evolution.AstorMain;
+import spoon.reflect.code.CtCodeElement;
+import spoon.reflect.declaration.CtType;
+/**
+ * 
+ * @author matias
+ *
+ */
+public class TypeCloneIngredientStrategyTestTest {
+
+	private String dep = new File("./examples/libs/junit-4.4.jar").getAbsolutePath();
+	private	File out = new File(ConfigurationProperties.getProperty("workingDirectory"));
+
+	protected Logger log = Logger.getLogger(this.getClass().getName());
+
+	@Test
+	public void testTypeCloneStrategyIsolatedMath70() throws Exception {
+		AstorMain main1 = new AstorMain();
+		ClassLoader classLoader = getClass().getClassLoader();
+		File learningDir = new File(classLoader.getResource("learningm70").getFile());
+
+		Class typeCloneGranularityClass = CtType.class;
+
+		String[] args = ExecutableCloneIngredientStrategyTest.createCommandM70(dep, out, learningDir, typeCloneGranularityClass);
+
+		log.debug(Arrays.toString(args));
+
+		main1.execute(args);
+		JGenProg engine = (JGenProg) main1.getEngine();
+
+		IngredientSpace ingredientSpace = engine.getIngredientStrategy().getIngredientSpace();
+
+		assertTrue(ingredientSpace.getLocations().size() > 0);
+
+		IngredientSearchStrategy ingStrategy = engine.getIngredientStrategy();
+
+		assertNotNull(ingStrategy);
+
+		assertTrue(ingStrategy instanceof CloneIngredientSearchStrategy<?>);
+
+		CloneIngredientSearchStrategy<CtType> cloneStrategy = (CloneIngredientSearchStrategy<CtType>) ingStrategy;
+
+		Optional<?> elements = cloneStrategy.queryelements();
+
+		assertNotNull(elements);
+
+		Map<String, ?> mapKeys = (Map<String, ?>) elements.get();
+
+		assertEquals(1, mapKeys.keySet().size());
+
+		// Let's take the Program Variant
+		ProgramVariant pvariant = engine.getVariants().get(0);
+
+		ModificationPoint buggyStatementModPoint = pvariant.getModificationPoints().get(0);
+
+		assertEquals(72, buggyStatementModPoint.getCodeElement().getPosition().getEndLine());
+
+		AstorOperator testOperator = new ReplaceOp(); // We dont care about the
+														// operator here
+
+		CtType buggyClass = buggyStatementModPoint.getCtClass();
+
+		// Important:I had to invoaue this method to execute computesimlist
+		// (it's private) before calling getfixspace()
+		Ingredient ingredientSelected = cloneStrategy.getFixIngredient(buggyStatementModPoint, testOperator);
+
+		assertNotNull(ingredientSelected);
+		
+		Queue<CtCodeElement> ingredientsToConsider = cloneStrategy.getfixspace(buggyStatementModPoint, testOperator,
+				buggyClass);
+
+		assertNotNull(ingredientsToConsider);
+
+		assertFalse(ingredientsToConsider.isEmpty());
+
+		//The suspicious class has 4 return statements.
+		assertEquals("Wrong number of ingredients collected", 4, ingredientsToConsider.size());
+
+		log.debug("Ingredients to considers: "+ ingredientsToConsider);
+		
+		Queue<CtCodeElement> ingredientsToConsiderInser = cloneStrategy.getfixspace(buggyStatementModPoint,new InsertAfterOp(),
+				buggyClass);
+		//the suspicious class has 22 statements (it does not include super calls)
+		assertEquals("Wrong number of ingredients collected", 22, ingredientsToConsiderInser.size());
+		
+	}
+}
