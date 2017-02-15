2a9fa93ffb372304a8e9edf5e30bbf70a019eda2
martinezmatias <matias.sebastian.martinez@gmail.com>
2017-02-15 14:55:17 +0100

    Changes in Process validation.
    We pass the timezone to the process we create.
    
    Moreover, we remove the list of ignore test cases and put as default
    that we do not include flaky test

--- src/main/java/fr/inria/astor/core/validation/executors/JUnitExecutorProcess.java
+++ src/main/java/fr/inria/astor/core/validation/executors/JUnitExecutorProcess.java
@@ -1,10 +1,12 @@
 package fr.inria.astor.core.validation.executors;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,28 +24,28 @@ import fr.inria.astor.junitexec.JUnitTestExecutor;
  * @author Matias Martinez, matias.martinez@inria.fr
  * 
  */
-public abstract class  JUnitExecutorProcess {
+public abstract class JUnitExecutorProcess {
 
 	protected Logger log = Logger.getLogger(Thread.currentThread().getName());
 	boolean avoidInterruption = false;
-	
-	public JUnitExecutorProcess(boolean avoidInterruption ) {
+
+	public JUnitExecutorProcess(boolean avoidInterruption) {
 		this();
 		this.avoidInterruption = avoidInterruption;
 	}
-	
+
 	public JUnitExecutorProcess() {
 		super();
 	}
 
-	public TestResult execute(String jvmPath,URL[] classpath, List<String> classesToExecute, int waitTime) {
-		return execute(jvmPath,urlArrayToString(classpath), classesToExecute, waitTime);
+	public TestResult execute(String jvmPath, URL[] classpath, List<String> classesToExecute, int waitTime) {
+		return execute(jvmPath, urlArrayToString(classpath), classesToExecute, waitTime);
 	}
 
 	public TestResult execute(String jvmPath, String classpath, List<String> classesToExecute, int waitTime) {
 		Process p = null;
 		jvmPath += File.separator + "java";
-		String systemcp = 	defineInitialClasspath();
+		String systemcp = defineInitialClasspath();
 
 		classpath = systemcp + File.pathSeparator + classpath;
 
@@ -52,6 +54,7 @@ public abstract class  JUnitExecutorProcess {
 		try {
 
 			List<String> command = new ArrayList<String>();
+
 			command.add(jvmPath);
 			command.add("-Xmx2048m");
 			command.add("-cp");
@@ -60,33 +63,57 @@ public abstract class  JUnitExecutorProcess {
 			command.addAll(cls);
 
 			printCommandToExecute(command);
-			
-			ProcessBuilder pb = new ProcessBuilder(command.toArray(new String[command.size()]));
+
+			// ProcessBuilder pb = new ProcessBuilder(command.toArray(new
+			// String[command.size()]));
+			ProcessBuilder pb = new ProcessBuilder("/bin/bash");
 			pb.redirectOutput();
 			pb.redirectErrorStream(true);
 			pb.directory(new File((ConfigurationProperties.getProperty("location"))));
 			long t_start = System.currentTimeMillis();
 			p = pb.start();
 
-			//WorkerThreadHelper worker = new WorkerThreadHelper(p);
-			//worker.start();
-			//worker.join(waitTime);
-			//log.debug("Launched for " + waitTime+" milliseconds");
-			p.waitFor(waitTime,TimeUnit.MILLISECONDS);
+			BufferedWriter p_stdin = new BufferedWriter(new OutputStreamWriter(p.getOutputStream()));
+
+			try {
+				//Set up the timezone
+				String timeZone = ConfigurationProperties.getProperty("timezone");
+				p_stdin.write("TZ=\"" + timeZone + "\"");
+				p_stdin.newLine();
+				p_stdin.flush();
+				p_stdin.write("export TZ");
+				p_stdin.newLine();
+				p_stdin.flush();
+				//Writing the command
+				p_stdin.write(toString(command));
+				p_stdin.newLine();
+				p_stdin.flush();
+				
+
+				// end
+				p_stdin.write("exit");
+				p_stdin.newLine();
+				p_stdin.flush();
+			} catch (IOException e) {
+				log.error(e);
+			}
+
+			//
+			p.waitFor(waitTime, TimeUnit.MILLISECONDS);
 			long t_end = System.currentTimeMillis();
 			log.debug("Execution time " + ((t_end - t_start) / 1000) + " seconds");
-		
-			if(!avoidInterruption){
+
+			if (!avoidInterruption) {
 				log.debug("Running Exit Value");
-				//We force obtaining the exit value.
+				// We force obtaining the exit value.
 				p.exitValue();
 			}
-			
+
 			TestResult tr = getTestResult(p);
 			p.destroy();
-			
+
 			return tr;
-		} catch ( IOException |InterruptedException |IllegalThreadStateException  ex) {
+		} catch (IOException | InterruptedException | IllegalThreadStateException ex) {
 			log.info("The Process that runs JUnit test cases had problems: " + ex.getMessage());
 			if (p != null)
 				p.destroy();
@@ -116,7 +143,8 @@ public abstract class  JUnitExecutorProcess {
 		}
 		return s;
 	}
-	protected String getProcessError(InputStream str){
+
+	protected String getProcessError(InputStream str) {
 		String out = "";
 		try {
 			BufferedReader in = new BufferedReader(new InputStreamReader(str));
@@ -130,14 +158,19 @@ public abstract class  JUnitExecutorProcess {
 		}
 		return out;
 	}
-	
+
 	private void printCommandToExecute(List<String> command) {
-		String commandString = command.toString().replace("[", "").replace("]", "").replace(",", " ");
+		String commandString = toString(command);
 		int trunk = ConfigurationProperties.getPropertyInt("commandTrunk");
 		String commandToPrint = (trunk != 0 && commandString.length() > trunk)
 				? (commandString.substring(0, trunk) + "..AND " + (commandString.length() - trunk) + " CHARS MORE...")
 				: commandString;
 		log.debug("Executing process: \n" + commandToPrint);
+	}
+
+	private String toString(List<String> command) {
+		String commandString = command.toString().replace("[", "").replace("]", "").replace(",", " ");
+		return commandString;
 	}
 
 }
