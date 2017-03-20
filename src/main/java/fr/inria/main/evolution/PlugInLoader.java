df517076d8359329b37c5bdbe844befbb3107191
martinezmatias <matias.sebastian.martinez@gmail.com>
2017-03-20 12:49:45 +0100

    Improving plugin loader:
    new methods for loading plugin + preventing NPE when a property is null

--- src/main/java/fr/inria/main/evolution/PlugInLoader.java
+++ src/main/java/fr/inria/main/evolution/PlugInLoader.java
@@ -1,8 +1,22 @@
 package fr.inria.main.evolution;
 
+import java.io.File;
+import java.util.List;
+
 import org.apache.log4j.Logger;
+
+import com.martiansoftware.jsap.JSAPException;
+
 import fr.inria.astor.core.loop.extension.AstorExtensionPoint;
+import fr.inria.astor.core.loop.spaces.ingredients.IngredientSpace;
+import fr.inria.astor.core.loop.spaces.ingredients.scopes.GlobalBasicIngredientSpace;
+import fr.inria.astor.core.loop.spaces.ingredients.scopes.LocalIngredientSpace;
+import fr.inria.astor.core.loop.spaces.ingredients.scopes.PackageBasicFixSpace;
+import fr.inria.astor.core.loop.spaces.operators.AstorOperator;
+import fr.inria.astor.core.loop.spaces.operators.OperatorSpace;
+import fr.inria.astor.core.manipulation.filters.AbstractFixSpaceProcessor;
 import fr.inria.astor.core.setup.ConfigurationProperties;
+
 /**
  * 
  * @author Matias Martinez
@@ -11,12 +25,16 @@ import fr.inria.astor.core.setup.ConfigurationProperties;
 public class PlugInLoader {
 
 	protected static Logger log = Logger.getLogger(Thread.currentThread().getName());
-	
+
 	public static AstorExtensionPoint loadPlugin(ExtensionPoints ep) throws Exception {
-		return loadPlugin(ConfigurationProperties.getProperty(ep.identifier), ep._class);
+		String property = ConfigurationProperties.getProperty(ep.identifier);
+		if(property == null || property.trim().isEmpty())
+			return null;
+		
+		return loadPlugin(property, ep._class);
 	}
-	
-	public  static AstorExtensionPoint loadPlugin(String className,Class type) throws Exception {
+
+	public static AstorExtensionPoint loadPlugin(String className, Class type) throws Exception {
 		Object object = null;
 		try {
 			Class classDefinition = Class.forName(className);
@@ -28,15 +46,21 @@ public class PlugInLoader {
 		if (type.isInstance(object))
 			return (AstorExtensionPoint) object;
 		else
-			throw new Exception(
-					"The strategy " + className + " does not extend from " +type.getClass().getName());
+			throw new Exception("The strategy " + className + " does not extend from " + type.getClass().getName());
 
 	}
-	public  static AstorExtensionPoint loadPlugin(ExtensionPoints ep, Class[] typesConst, Object[] args) throws Exception{
-		return loadPlugin(ConfigurationProperties.getProperty(ep.identifier), ep._class, typesConst, args);
+
+	public static AstorExtensionPoint loadPlugin(ExtensionPoints ep, Class[] typesConst, Object[] args)
+			throws Exception {
+		String property = ConfigurationProperties.getProperty(ep.identifier);
+		if(property == null || property.trim().isEmpty())
+			return null;
+		
+		return loadPlugin(property, ep._class, typesConst, args);
 	}
-	
-	public  static AstorExtensionPoint loadPlugin(String className,Class type, Class[] typesConst, Object[] args) throws Exception {
+
+	public static AstorExtensionPoint loadPlugin(String className, Class type, Class[] typesConst, Object[] args)
+			throws Exception {
 		Object object = null;
 		try {
 			Class classDefinition = Class.forName(className);
@@ -48,9 +72,57 @@ public class PlugInLoader {
 		if (type.isInstance(object))
 			return (AstorExtensionPoint) object;
 		else
-			throw new Exception(
-					"The strategy " + className + " does not extend from " +type.getClass().getName());
+			throw new Exception("The strategy " + className + " does not extend from " + type.getClass().getName());
 
 	}
+
+	public static OperatorSpace loadOperatorSpace() throws Exception {
+		// We check if the user defines the operators to include in the operator
+		// space
+		OperatorSpace operatorSpace = null;
+		String customOp = ConfigurationProperties.getProperty("customop");
+		if (customOp != null && !customOp.isEmpty()) {
+			operatorSpace = createCustomOperatorSpace(customOp);
+		} else {
+			customOp = ConfigurationProperties.getProperty("operatorspace");
+			if (customOp != null && !customOp.isEmpty())
+				operatorSpace = (OperatorSpace) PlugInLoader.loadPlugin(ExtensionPoints.OPERATORS_SPACE);
+		}
+		return operatorSpace;
+	}
 	
+	public static IngredientSpace loadIngredientSpace(List<AbstractFixSpaceProcessor<?>> ingredientProcessors)
+			throws JSAPException, Exception {
+		// The ingredients for build the patches
+		String scope = ConfigurationProperties.properties.getProperty("scope");
+		IngredientSpace ingredientspace = null;
+		if ("global".equals(scope)) {
+			ingredientspace = (new GlobalBasicIngredientSpace(ingredientProcessors));
+		} else if ("package".equals(scope)) {
+			ingredientspace = (new PackageBasicFixSpace(ingredientProcessors));
+		} else if ("local".equals(scope)) {
+			ingredientspace = (new LocalIngredientSpace(ingredientProcessors));
+		} else {
+			ingredientspace = (IngredientSpace) PlugInLoader.loadPlugin(ExtensionPoints.INGREDIENT_STRATEGY_SCOPE,
+					new Class[] { List.class }, new Object[] { ingredientProcessors });
+
+		}
+		return ingredientspace;
+	}
+
+	private static OperatorSpace createCustomOperatorSpace(String customOp) throws Exception {
+		OperatorSpace customSpace = new OperatorSpace();
+		String[] operators = customOp.split(File.pathSeparator);
+		for (String op : operators) {
+			AstorOperator aop = (AstorOperator) PlugInLoader.loadPlugin(op, ExtensionPoints.CUSTOM_OPERATOR._class);
+			if (aop != null)
+				customSpace.register(aop);
+		}
+		if (customSpace.getOperators().isEmpty()) {
+			log.error("Empty custom operator space");
+			throw new Exception("Empty custom operator space");
+		}
+		return customSpace;
+	}
+
 }
