fc25d2e3c8218a1a985b156c938efbb73f4f5efa
martinezmatias <matias.sebastian.martinez@gmail.com>
2017-08-28 14:45:33 +0200

    [Minor, doc] Documenting enum

--- src/main/java/fr/inria/main/AstorOutputStatus.java
+++ src/main/java/fr/inria/main/AstorOutputStatus.java
@@ -1,9 +1,20 @@
 package fr.inria.main;
+
 /**
  * Output values of Astor
+ * 
  * @author Matias Martinez
  *
  */
 public enum AstorOutputStatus {
-	TIME_OUT, ERROR, CONVERGED, MAX_GENERATION, STOP_BY_PATCH_FOUND;
+	// max time reached
+	TIME_OUT,
+	// max number of generation reached
+	MAX_GENERATION,
+	// max number of solution reached.
+	STOP_BY_PATCH_FOUND,
+	// An exception was thrown
+	ERROR,
+	// stops due to a convergence on the search
+	CONVERGED;
 }
