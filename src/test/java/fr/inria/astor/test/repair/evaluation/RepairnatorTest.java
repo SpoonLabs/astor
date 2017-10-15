34139d1e0ee79692c7a902395297a33ced983531
martinezmatias <matias.sebastian.martinez@gmail.com>
2017-10-15 20:45:29 +0200

    [test] new test for testing projects with package-info.java files

--- src/test/java/fr/inria/astor/test/repair/evaluation/RepairnatorTest.java
+++ src/test/java/fr/inria/astor/test/repair/evaluation/RepairnatorTest.java
@@ -56,6 +56,46 @@ public class RepairnatorTest {
 	}
 
 	@Test
+	public void testjsoup285353482WithPackageInfo() throws Exception {
+		String m2path = System.getenv("HOME") + "/.m2/repository/";
+		File fm2 = new File(m2path);
+		if (!fm2.exists()) {
+			throw new Exception(m2path + "does not exit");
+		}
+
+		String[] command = new String[] { "-dependencies",
+				m2path + "/junit/junit/4.12/junit-4.12.jar:" + m2path
+						+ "/org/hamcrest/hamcrest-core/1.3/hamcrest-core-1.3.jar:" + m2path
+						+ "/com/google/code/gson/gson/2.7/gson-2.7.jar",
+				"-mode", "jgenprog", "-location",
+				new File("./examples/librepair-experiments-jhy-jsoup-285353482-20171009-062400_bugonly_with_package_info")
+						.getAbsolutePath(),
+				"-srcjavafolder", "/src/main/java", // Modified by M.M, original
+													// path was absolute.
+				"-stopfirst", "true", "-population", "1", "-maxgen", "50", // Added
+																			// by
+																			// M.M.,
+																			// not
+																			// present
+																			// in
+																			// command
+																			// sent
+																			// by
+																			// S.U.
+				"-parameters", "timezone:Europe/Paris:maxnumbersolutions:3:limitbysuspicious:false:" + "loglevel:DEBUG",
+				"-maxtime", "100", "-seed", "1" };
+		CommandSummary cs = new CommandSummary(command);
+		AstorMain main1 = new AstorMain();
+		main1.execute(cs.flat());
+
+		assertTrue(main1.getEngine().getVariants().size() > 0);
+		assertTrue(main1.getEngine().getVariants().get(0).getModificationPoints().size() > 0);
+
+	}
+
+	
+	
+	@Test
 	@Ignore
 	public void testAsc() throws Exception {
 		// TODO.
