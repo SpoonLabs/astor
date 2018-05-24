ead1d977070aae47499e004a33bbd3e087aefc4c
martinezmatias <matias.sebastian.martinez@gmail.com>
2018-05-24 14:56:10 +0200

    adding toString

--- src/main/java/fr/inria/astor/core/manipulation/synthesis/DynamicCollectedValues.java
+++ src/main/java/fr/inria/astor/core/manipulation/synthesis/DynamicCollectedValues.java
@@ -8,12 +8,13 @@ import fr.inria.lille.repair.common.Candidates;
 
 /**
  * Encapsulates the collected data.
+ * 
  * @author Matias Martinez
  *
  */
 public class DynamicCollectedValues {
-	//Key: test name
-	//Value: list of executions
+	// Key: test name
+	// Value: list of executions
 	Map<String, List<Candidates>> values;
 
 	public DynamicCollectedValues() {
@@ -32,7 +33,32 @@ public class DynamicCollectedValues {
 	public void setValues(Map<String, List<Candidates>> values) {
 		this.values = values;
 	}
-	public List<Candidates> getDataValuesFromTest(String testname){
+
+	public List<Candidates> getDataValuesFromTest(String testname) {
 		return values.get(testname);
 	}
+
+	public String toString() {
+		String out = "";
+		int nrtest = 0;
+		for (String key : values.keySet()) {
+			out += ("test " + nrtest++ + " :" + key) + "\n";
+			List<Candidates> executions = values.get(key);
+			out += ("Total nr executions done by test: " + executions.size()) + "\n";
+			int i = 0;
+			for (Candidates candidates2 : executions) {
+				out += ("--Total nr of vars " + (i++) + ": " + candidates2.size()) + "\n";
+				int j = 0;
+				for (fr.inria.lille.repair.expression.Expression expression : candidates2) {
+
+					out += ("--*-->" + i + " " + (j++) + " " + expression.asPatch() + " " + expression.getValue())
+							+ "\n";
+
+				}
+			}
+
+		}
+		return out;
+	}
+
 }
