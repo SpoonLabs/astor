8f26d571533d7ff43a702229fad91dd515f04df8
martinezmatias <matias.sebastian.martinez@gmail.com>
2018-08-30 08:59:09 +0200

    [test] new test of extension point ingredient target

--- src/test/java/fr/inria/astor/test/repair/evaluation/extensionpoints/ingredients/IngredientProcessorTest.java
+++ src/test/java/fr/inria/astor/test/repair/evaluation/extensionpoints/ingredients/IngredientProcessorTest.java
@@ -1,5 +1,7 @@
 package fr.inria.astor.test.repair.evaluation.extensionpoints.ingredients;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
@@ -12,10 +14,16 @@ import fr.inria.astor.approaches.jgenprog.JGenProg;
 import fr.inria.astor.core.entities.Ingredient;
 import fr.inria.astor.core.entities.ModificationPoint;
 import fr.inria.astor.core.entities.ProgramVariant;
+import fr.inria.astor.core.ingredientbased.IngredientBasedEvolutionaryRepairApproachImpl;
+import fr.inria.astor.core.manipulation.filters.ExpressionBooleanIngredientSpaceProcessor;
 import fr.inria.astor.core.manipulation.filters.ExpressionIngredientSpaceProcessor;
 import fr.inria.astor.core.manipulation.filters.MethodInvocationFixSpaceProcessor;
 import fr.inria.astor.core.solutionsearch.spaces.ingredients.IngredientPoolLocationType;
+import fr.inria.astor.core.solutionsearch.spaces.ingredients.scopes.AstorCtIngredientPool;
+import fr.inria.astor.core.solutionsearch.spaces.operators.demo.InvocationFixSpaceProcessor;
+import fr.inria.astor.core.solutionsearch.spaces.operators.demo.NullPreconditionOperatorMI;
 import fr.inria.astor.test.repair.evaluation.regression.MathCommandsTests;
+import fr.inria.main.AstorOutputStatus;
 import fr.inria.main.CommandSummary;
 import fr.inria.main.evolution.AstorMain;
 import fr.inria.main.evolution.ExtensionPoints;
@@ -127,6 +135,43 @@ public class IngredientProcessorTest {
 		checkModificationPointTypes(variantss, CtStatement.class);
 		checkIngredientTypes(ingSpace, CtExpression.class);
 
+	}
+
+	@Test
+	public void test1PreconditionWithExpressionIngredientOnMethodInvocation() throws Exception {
+
+		CommandSummary command = MathCommandsTests.getMath85Command();
+		command.command.put("-mode", "jgenprog");
+		command.command.put("-maxgen", "10");
+		command.command.put("-loglevel", "DEBUG");
+		command.command.put("-scope", "local");
+		command.command.put("-stopfirst", "true");
+		command.command.put("-flthreshold", "0.5");
+		command.command.put(ExtensionPoints.REPAIR_OPERATORS.argument(), NullPreconditionOperatorMI.class.getName());
+		command.command.put("-out", new File("./output_astor/demo").getAbsolutePath());
+		command.command.put("-saveall", "true");
+		command.command.put(ExtensionPoints.TARGET_CODE_PROCESSOR.argument(),
+				InvocationFixSpaceProcessor.class.getCanonicalName());
+		command.command.put(ExtensionPoints.TARGET_INGREDIENT_CODE_PROCESSOR.argument(),
+				ExpressionBooleanIngredientSpaceProcessor.class.getName());
+		AstorMain main = new AstorMain();
+		main.execute(command.flat());
+
+		IngredientBasedEvolutionaryRepairApproachImpl approach = (IngredientBasedEvolutionaryRepairApproachImpl) main
+				.getEngine();
+		List<ModificationPoint> modificationPoints = approach.getVariants().get(0).getModificationPoints();
+		for (ModificationPoint modificationPoint : modificationPoints) {
+			assertTrue("type: " + modificationPoint.getCodeElement(),
+					modificationPoint.getCodeElement() instanceof CtInvocation);
+		}
+		AstorCtIngredientPool pool = (AstorCtIngredientPool) approach.getIngredientPool();
+		for (Ingredient ingredient : pool.getAllIngredients()) {
+			assertTrue(ingredient.getCode() instanceof CtExpression);
+			CtExpression expr = (CtExpression) ingredient.getCode();
+			assertEquals("boolean", expr.getType().unbox().toString());
+		}
+
+		assertNotEquals(AstorOutputStatus.ERROR, main.getEngine().getOutputStatus());
 	}
 
 	public void checkModificationPointTypes(List<ProgramVariant> variantss, Class classToProcess) {
