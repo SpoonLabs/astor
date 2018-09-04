c8a401752590dd173f04d86c37b06368b64021f0
martinezmatias <matias.sebastian.martinez@gmail.com>
2018-09-04 12:57:31 +0200

    [test, bugfix] repairing tests: avoiding absolute path on src or bin parameters

--- src/test/java/fr/inria/astor/test/repair/evaluation/regression/RepairnatorTest.java
+++ src/test/java/fr/inria/astor/test/repair/evaluation/regression/RepairnatorTest.java
@@ -18,6 +18,7 @@ import fr.inria.astor.core.setup.ConfigurationProperties;
 import fr.inria.astor.core.solutionsearch.spaces.ingredients.scopes.IngredientPoolScope;
 import fr.inria.main.CommandSummary;
 import fr.inria.main.evolution.AstorMain;
+import junit.framework.Assert;
 
 /**
  * failing projects sent by Simon
@@ -114,16 +115,15 @@ public class RepairnatorTest {
 		int generations = 500;
 		// Changing the location of source folders
 		String[] args = new String[] { "-dependencies", dep, "-mode", "jgenprog", "-location",
-				new File("./examples/math_20").getAbsolutePath(), "-package", "org.apache.commons", 
-				//Abstracts (refering to another folder)
-				"-srcjavafolder",new File("./examples/math_70").getAbsolutePath() + "/src/main/java/", //
+				new File("./examples/math_20").getAbsolutePath(), "-package", "org.apache.commons",
+				// Absolute path
+				"-srcjavafolder", new File("./examples/math_70").getAbsolutePath() + "/src/main/java/", //
 				"-srctestfolder", new File("./examples/math_70").getAbsolutePath() + "/src/test/java/", //
-				"-binjavafolder", new File("./examples/math_70").getAbsolutePath() +"/target/classes", 
-				"-bintestfolder", new File("./examples/math_70").getAbsolutePath() +"/target/test-classes", //
-				"-javacompliancelevel",
-				"7", "-flthreshold", "0.5", "-out", out.getAbsolutePath(), "-scope", "local", "-seed", "10", "-maxgen",
-				Integer.toString(generations), "-stopfirst", "true", "-maxtime", "100", "-loglevel", "INFO",
-				"-parameters", "disablelog:false"
+				"-binjavafolder", new File("./examples/math_70").getAbsolutePath() + "/target/classes",
+				"-bintestfolder", new File("./examples/math_70").getAbsolutePath() + "/target/test-classes", //
+				"-javacompliancelevel", "7", "-flthreshold", "0.5", "-out", out.getAbsolutePath(), "-scope", "local",
+				"-seed", "10", "-maxgen", Integer.toString(generations), "-stopfirst", "true", "-maxtime", "100",
+				"-loglevel", "INFO", "-parameters", "disablelog:false"
 
 		};
 
@@ -134,20 +134,15 @@ public class RepairnatorTest {
 		cs.command.put("-saveall", "true");
 
 		System.out.println(Arrays.toString(cs.flat()));
-		main1.execute(cs.flat());
-
-		List<ProgramVariant> solutions = main1.getEngine().getSolutions();
-		assertTrue(solutions.size() > 0);
-		assertEquals(1, solutions.size());
-		ProgramVariant variant = solutions.get(0);
-
-		OperatorInstance mi = variant.getOperations().values().iterator().next().get(0);
-		assertNotNull(mi);
-		assertEquals(IngredientPoolScope.LOCAL, mi.getIngredientScope());
-
-		assertEquals("return solve(f, min, max)", mi.getModified().toString());
+		try {
+			main1.execute(cs.flat());
+			Assert.fail("The absolute path must not work");
+		} catch (Exception e) {
+			// we expect an exception
+		}
 
 	}
+
 	@Test
 	public void testMath70WrongPaths() throws Exception {
 		AstorMain main1 = new AstorMain();
@@ -155,16 +150,15 @@ public class RepairnatorTest {
 		File out = new File(ConfigurationProperties.getProperty("workingDirectory"));
 		int generations = 500;
 		String[] args = new String[] { "-dependencies", dep, "-mode", "jgenprog", "-location",
-				new File("./examples/math_70").getAbsolutePath(), "-package", "org.apache.commons", 
-				//Abstracts (refering to another folder)
-				"-srcjavafolder",new File("./examples/math_70").getAbsolutePath() + "/src/main/java/", //
+				new File("./examples/math_70").getAbsolutePath(), "-package", "org.apache.commons",
+				// Abstracts (refering to another folder)
+				"-srcjavafolder", new File("./examples/math_70").getAbsolutePath() + "/src/main/java/", //
 				"-srctestfolder", new File("./examples/math_70").getAbsolutePath() + "/src/test/java/", //
-				"-binjavafolder", new File("./examples/math_70").getAbsolutePath() +"/target/classes", 
-				"-bintestfolder", new File("./examples/math_70").getAbsolutePath() +"/target/test-classes", //
-				"-javacompliancelevel",
-				"7", "-flthreshold", "0.5", "-out", out.getAbsolutePath(), "-scope", "local", "-seed", "10", "-maxgen",
-				Integer.toString(generations), "-stopfirst", "true", "-maxtime", "100", "-loglevel", "INFO",
-				"-parameters", "disablelog:false"
+				"-binjavafolder", new File("./examples/math_70").getAbsolutePath() + "/target/classes",
+				"-bintestfolder", new File("./examples/math_70").getAbsolutePath() + "/target/test-classes", //
+				"-javacompliancelevel", "7", "-flthreshold", "0.5", "-out", out.getAbsolutePath(), "-scope", "local",
+				"-seed", "10", "-maxgen", Integer.toString(generations), "-stopfirst", "true", "-maxtime", "100",
+				"-loglevel", "INFO", "-parameters", "disablelog:false"
 
 		};
 
@@ -173,24 +167,25 @@ public class RepairnatorTest {
 		cs.command.put("-stopfirst", "true");
 		cs.command.put("-loglevel", "DEBUG");
 		cs.command.put("-saveall", "true");
-		
+
 		//
 		cs.command.put("-binjavafolder", "blabla");
 
 		System.out.println(Arrays.toString(cs.flat()));
-		try{
-			
-		main1.execute(cs.flat());
+		try {
+
+			main1.execute(cs.flat());
 			fail("expected ex wrong bin folder");
-		}catch (Exception e) {
-			//expected
+		} catch (Exception e) {
+			// expected
 		}
-		cs.command.put("-binjavafolder", new File("./examples/math_70").getAbsolutePath() +"/target/classes");
-		//wrong path but astor resolves it
-		cs.command.put("-srcjavafolder", new File("./examples/math_70").getAbsolutePath() +"/blabla1");
+		cs.command.put("-binjavafolder", "/target/classes");
+		cs.command.put("-bintestfolder", "/target/test-classes");
+		// wrong path but astor resolves it
+		cs.command.put("-srcjavafolder", new File("./examples/math_70").getAbsolutePath() + "/blabla1");
 
 		main1.execute(cs.flat());
-		
+
 		List<ProgramVariant> solutions = main1.getEngine().getSolutions();
 		assertTrue(solutions.size() > 0);
 		assertEquals(1, solutions.size());
