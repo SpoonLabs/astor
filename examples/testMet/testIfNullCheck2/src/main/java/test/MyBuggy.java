35165ce17900621d2a12b871a5797e2977a19c81
martinezmatias <matias.sebastian.martinez@gmail.com>
2019-01-09 13:50:52 +0100

    add testing examples

---
+++ examples/testMet/testIfNullCheck2/src/main/java/test/MyBuggy.java
@@ -0,0 +1,36 @@
+package test;
+
+public class MyBuggy {
+
+	public Integer operation(int i1, int i2, String type) {
+
+		if ("*".equals(type)) {
+			return i1 * i2;
+
+		}
+
+		if ("+".equals(type)) {
+			return i1 + i2;
+
+		}
+
+		if ("-".equals(type)) {
+			return i1 - i2;
+
+		}
+
+		if ("gr".equals(type)) {
+
+			if (i2 > i1)
+				return i2;
+
+			if (i1 >= i2)
+				return i1;
+
+		}
+		System.out.println(type.toString());// NPE if type == null
+
+		return null;
+	}
+
+}
