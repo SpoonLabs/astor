e6f7cac3bf14eeb9104f4df8df81091f67109f5f
martinezmatias <matias.sebastian.martinez@gmail.com>
2019-06-24 11:19:01 +0200

    including quixbugs test on travis

--- src/test/java/fr/inria/astor/test/repair/QuixBugsRepairTest.java
+++ src/test/java/fr/inria/astor/test/repair/QuixBugsRepairTest.java
@@ -65,9 +65,9 @@ public class QuixBugsRepairTest {
 		cs.command.put("-loglevel", "DEBUG");
 		cs.command.put("-stopfirst", "TRUE");
 		cs.command.put("-parameters", "logtestexecution:TRUE:"
-				+ "disablelog:TRUE:maxtime:120:autocompile:true:gzoltarpackagetonotinstrument:com.google.gson_engine"
+				+ "disablelog:TRUE:maxtime:120:autocompile:false:gzoltarpackagetonotinstrument:com.google.gson_engine"
 				+ GZoltarFaultLocalization.PACKAGE_SEPARATOR + "java_programs_test");
-		cs.command.put("-location", new File("./examples/quixbugs/" + name).getAbsolutePath());
+		cs.command.put("-location", new File("./examples/quixbugscompiled/" + name).getAbsolutePath());
 
 		return cs;
 	}
@@ -256,6 +256,7 @@ public class QuixBugsRepairTest {
 	 * @throws Exception
 	 */
 	@Test
+	@Ignore
 	public void test_get_factorsRepair_cardumen() throws Exception {
 		AstorMain main1 = new AstorMain();
 
