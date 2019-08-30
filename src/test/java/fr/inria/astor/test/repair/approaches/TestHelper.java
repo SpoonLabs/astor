a37bfc864d0f3da97a6231e404ff3270f0fc47a6
martinezmatias <matias.sebastian.martinez@gmail.com>
2019-08-30 14:07:27 +0200

    repairing var resolver

--- src/test/java/fr/inria/astor/test/repair/approaches/TestHelper.java
+++ src/test/java/fr/inria/astor/test/repair/approaches/TestHelper.java
@@ -8,10 +8,10 @@ package fr.inria.astor.test.repair.approaches;
 public class TestHelper {
 
 	public static boolean equalsNoParentesis(String s1, String s2) {
-		return getWithoutParenth(s1).equals(getWithoutParenth(s2));
+		return getWithoutParentheses(s1).equals(getWithoutParentheses(s2));
 	}
 
-	public static String getWithoutParenth(String s1) {
+	public static String getWithoutParentheses(String s1) {
 		return s1.replace("(", "").replace(")", "");
 	}
 }
