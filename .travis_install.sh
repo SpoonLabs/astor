35ce77eac5feaab604beb0e64097ec7e3c67178b
Jian GU <ycaxgjd@gmail.com>
2019-09-03 15:17:26 +0200

    make `compilemaven` and `compile` quiet in correct way

--- .travis_install.sh
+++ .travis_install.sh
@@ -6,7 +6,7 @@ function compile {
     if [[ ! -d "$1target/classes" ]]; then
     	echo compiling $1
         cd $1
-        mvn test -DskipTests
+        mvn -ff -q test -DskipTests
         cd ../../
     fi
 }
@@ -14,8 +14,8 @@ function compile {
 function compilef {
 		echo compilingf $1
         cd $1
-        mvn clean
-        mvn install -DskipTests
+        mvn -ff -q clean
+        mvn -ff -q install -DskipTests
         cd ../../
 }
 
@@ -31,9 +31,9 @@ function compilemaven {
 	     if [[ -f "$FILE/pom.xml" ]]; then
     	    cd $FILE
     	    echo in dir `pwd`
-    	    mvn clean
+    	    mvn -ff -q clean
     	    echo after claning $FILE
-        	mvn test -DskipTests
+        	mvn -ff -q test -DskipTests
         	cd ../../../
     	fi 
 	done
@@ -42,28 +42,28 @@ function compilemaven {
 
 
 echo calling compile maven
-compilemaven -q "examples/testMultiMet/"
-compilemaven -q "examples/testMet/"
+compilemaven "examples/testMultiMet/"
+compilemaven "examples/testMet/"
 
-compile -q "examples/Math-0c1ef/"
-compile -q "examples/math_85/"
-compile -q "examples/math_70/"
-compile -q "examples/Math-issue-280/"
-compile -q "examples/Math-issue-288/"
-compile -q "examples/math_2/"
-compile -q "examples/math_5/"
-compile -q "examples/jsoup31be24/"
-compile -q "examples/introclass/3b2376/003/"
-compile -q "examples/math_50/"
-compile -q "examples/math_74/"
-compile -q "examples/math_76/"
-compile -q "examples/math_106/"
-compile -q "examples/lang_63/"
-compile -q "examples/lang_39/"
-compile -q "examples/lang_1/"
-compile -q "examples/lang_55/"
-compile -q "examples/math_57/"
-compile -q "examples/math_70_modified/"
-compile -q "examples/lang_7/"
+compile "examples/Math-0c1ef/"
+compile "examples/math_85/"
+compile "examples/math_70/"
+compile "examples/Math-issue-280/"
+compile "examples/Math-issue-288/"
+compile "examples/math_2/"
+compile "examples/math_5/"
+compile "examples/jsoup31be24/"
+compile "examples/introclass/3b2376/003/"
+compile "examples/math_50/"
+compile "examples/math_74/"
+compile "examples/math_76/"
+compile "examples/math_106/"
+compile "examples/lang_63/"
+compile "examples/lang_39/"
+compile "examples/lang_1/"
+compile "examples/lang_55/"
+compile "examples/math_57/"
+compile "examples/math_70_modified/"
+compile "examples/lang_7/"
 
 
