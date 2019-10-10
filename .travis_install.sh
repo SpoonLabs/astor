a648f9ad404b8e7f015e4d933978b4d836211eb1
Matias Martinez <martinezmatias@users.noreply.github.com>
2019-10-10 11:06:47 +0200

    comming back to the root dir after compile (#198)

--- .travis_install.sh
+++ .travis_install.sh
@@ -66,6 +66,8 @@ compile "examples/math_57/"
 compile "examples/math_70_modified/"
 compile "examples/lang_7/"
 compile "examples/issues/LeapYearIssue196/"
+cd ../
 compile "examples/issues/LeapYearIssue196-bis/"
+cd ../
 
 
