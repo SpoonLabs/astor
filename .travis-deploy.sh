477db9df9622c4a2af74647c585e1c06a85adff0
martinezmatias <matias.sebastian.martinez@gmail.com>
2019-12-18 10:12:03 +0100

    up2

--- .travis-deploy.sh
+++ .travis-deploy.sh
@@ -1,6 +1,6 @@
 if [[ $TRAVIS_BRANCH == "deploy"]]; then
 		echo "before deploy" 
         mvn versions:set -DnewVersion=`git rev-parse HEAD`
-		mvn -ff -q  deploy --settings .travis-settings.xml -DskipTests=true -B;
+		mvn deploy --settings .travis-settings.xml -DskipTests=true -B;
 		echo "Deployed"
 fi
